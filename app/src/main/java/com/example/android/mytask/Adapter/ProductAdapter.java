88b517d4bcb5d8cff511a24fe3b055cfd5942590
Ibrahimadhraf <33137985+Ibrahimadhraf@users.noreply.github.com>
2019-04-15 14:58:13 +0200

    git

--- app/src/main/java/com/example/android/mytask/Adapter/ProductAdapter.java
+++ app/src/main/java/com/example/android/mytask/Adapter/ProductAdapter.java
@@ -21,7 +21,7 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.productViewHolder> {
-   private Context context;
+    protected Context context;
    private List<ProductsRate>productList;
 
     public ProductAdapter(Context context, List<ProductsRate> productList) {
@@ -40,30 +40,50 @@ public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.productV
     //here we hold the view item with data from the a
     @Override
     public void onBindViewHolder(@NonNull productViewHolder holder, int position) {
-        String name=productList.get(position).getProduct().getName();
+        String name=productList.get(0).getProduct().getName();
         if(name!=null&&name.isEmpty()) {
             holder.titleTextView.setText(name);
+        }else {
+            holder.titleTextView.setText(String.valueOf(0));
         }
         String price=productList.get(position).getPrice();
         if(price!=null&&price.isEmpty()) {
             holder.priceTextView.setText(price);
         }
+        else {
+           holder.priceTextView.setText(String.valueOf(0));
+        }
 
-        String amout=productList.get(position).getAmount();
+        String amout=productList.get(0).getAmount();
         if(amout!=null&&amout.isEmpty()) {
             holder.amountTextView.setText(amout);
+        }else {
+            holder.amountTextView.setText(String.valueOf(0));
         }
-         int count=productList.get(position).orderdetailsList.get(position).getCount();
-         holder.countTextView.setText(String.valueOf(count));
-       String url_image=productList.get(position).getProduct().getPhoto_urlList().get(position).getImage_url();
+
+         String count=productList.get(position).orderdetailsList.get(0).getCount();
+        if(count != null &&count.isEmpty()) {
+            holder.countTextView.setText(String.valueOf(count));
+        }else {
+            holder.countTextView.setText(String.valueOf(0));
+        }
+       String url_image=productList.get(position).getProduct().getPhoto_urlList().get(0).getImage_url();
        if(url_image !=null&&url_image.isEmpty()) {
            Uri uri = Uri.parse(url_image);
            Picasso.get().load(uri).into(holder.imageProduct);
        }
-        int star=productList.get(position).product.rating_totals.get(position).stars;
-        holder.ratingBar.setNumStars(star);
-        int rating=productList.get(position).product.rating_totals.get(position).count;
-        holder.titleTextView.setText(rating);
+        String star=productList.get(position).getProduct().getRating_totals().get(0).getStars();
+       if(star!=null&&star.isEmpty()) {
+           holder.ratingBar.setRating(Integer.parseInt(star));
+       }else {
+           holder.ratingBar.setRating(0);
+       }
+        String rating=productList.get(position).product.rating_totals.get(0).count;
+       if(star!=null&&star.isEmpty()) {
+           holder.titleTextView.setText(rating);
+       }else {
+           holder.titleTextView.setText(String.valueOf(0));
+       }
     }
 
     @Override
