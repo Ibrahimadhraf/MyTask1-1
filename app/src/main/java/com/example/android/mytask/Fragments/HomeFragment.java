12243daec33b56782eef1e58f74fdb3b42ed262f
Ibrahimadhraf <33137985+Ibrahimadhraf@users.noreply.github.com>
2019-04-15 17:52:31 +0200

    git

--- app/src/main/java/com/example/android/mytask/Fragments/HomeFragment.java
+++ app/src/main/java/com/example/android/mytask/Fragments/HomeFragment.java
@@ -1,15 +1,77 @@
 package com.example.android.mytask.Fragments;
 
 import android.os.Bundle;
-import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.android.mytask.Adapter.DataAdapter;
+import com.example.android.mytask.Adapter.ProductAdapter;
+import com.example.android.mytask.Model.Data;
+import com.example.android.mytask.Model.MainData;
+import com.example.android.mytask.Model.ProductsRate;
 import com.example.android.mytask.R;
+import com.example.android.mytask.ViewModel.DataViewModel;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
 
 public class HomeFragment extends Fragment {
-    public View onCreateView( LayoutInflater inflater,  ViewGroup container,  Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.homefragment, container, false);
+    //view model
+    private DataViewModel viewModel;
+    //data recycler view
+   private RecyclerView mDataRecyclerView;
+    //prouduct recyclerView
+   private RecyclerView mProductRecyclerView;
+    private CompositeDisposable disposable=new CompositeDisposable();
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.homefragment, container, false);
+      mDataRecyclerView =view.findViewById(R.id.data_recyclerView);
+        mDataRecyclerView.setHasFixedSize(true);
+        mDataRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        mProductRecyclerView=view.findViewById(R.id.recyclerView2);
+        mProductRecyclerView.setHasFixedSize(true);
+        mProductRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        viewModel= ViewModelProviders.of(this).get(DataViewModel.class);
+        fetchdata();
+        return view;
+    }
+    private void fetchdata() {
+        disposable.add(
+                viewModel.client.fetchData(getContext())
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe(new Consumer<MainData>() {
+                            @Override
+                            public void accept(MainData mainData)  {
+                                List<Data> data=mainData.getData();
+                                DataAdapter adapter=new DataAdapter(data,getContext());
+                                adapter.notifyDataSetChanged();
+                                mDataRecyclerView.setAdapter(adapter);
+                                List<ProductsRate> products=mainData.getProductsRates();
+                                ProductAdapter madapter=new ProductAdapter(getContext(),products);
+                                mProductRecyclerView.setAdapter(madapter);
+                                madapter.notifyDataSetChanged();
+                            }
+
+                        }));
+
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        disposable.clear();
     }
 }
--- app/src/main/java/com/example/android/mytask/MainActivity.java
+++ app/src/main/java/com/example/android/mytask/MainActivity.java
@@ -5,54 +5,26 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.example.android.mytask.Adapter.DataAdapter;
-import com.example.android.mytask.Adapter.ProductAdapter;
 import com.example.android.mytask.Fragments.BuyingFragment;
 import com.example.android.mytask.Fragments.HomeFragment;
 import com.example.android.mytask.Fragments.NotificationFragment;
 import com.example.android.mytask.Fragments.OffersFragment;
-import com.example.android.mytask.Model.Data;
-import com.example.android.mytask.Model.MainData;
-import com.example.android.mytask.Model.ProductsRate;
-import com.example.android.mytask.ViewModel.DataViewModel;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-
-import java.util.List;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProviders;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.disposables.CompositeDisposable;
-import io.reactivex.functions.Consumer;
-import io.reactivex.schedulers.Schedulers;
 
 
 public class MainActivity extends AppCompatActivity {
-    //view model
-    public DataViewModel viewModel;
-    //data recycler view
-    RecyclerView mDataRecyclerView;
-    //prouduct recyclerView
-    RecyclerView mProductRecyclerView;
-    CompositeDisposable disposable=new CompositeDisposable();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         BottomNavigationView bottomNavigationView = findViewById(R.id.navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(listener);
-        mDataRecyclerView=findViewById(R.id.data_recyclerView);
-        mDataRecyclerView.setHasFixedSize(true);
-        mDataRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-        mProductRecyclerView=findViewById(R.id.recyclerView2);
-        mProductRecyclerView.setHasFixedSize(true);
-        mProductRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-         viewModel= ViewModelProviders.of(this).get(DataViewModel.class);
-         fetchdata();
+
     }
 //add navigation button
     private BottomNavigationView.OnNavigationItemSelectedListener listener =
@@ -85,41 +57,18 @@ public class MainActivity extends AppCompatActivity {
                                 }
                             };
 
-    private void fetchdata() {
-        disposable.add(
-                viewModel.client.fetchData(MainActivity.this)
-                        .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Consumer<MainData>() {
-                    @Override
-                    public void accept(MainData mainData)  {
-                       List<Data> data=mainData.getData();
-                        DataAdapter adapter=new DataAdapter(data,MainActivity.this);
-                        adapter.notifyDataSetChanged();
-                        mDataRecyclerView.setAdapter(adapter);
-                        List<ProductsRate> products=mainData.getProductsRates();
-                        ProductAdapter madapter=new ProductAdapter(MainActivity.this,products);
-                        mProductRecyclerView.setAdapter(madapter);
-                        madapter.notifyDataSetChanged();
-                    }
-
-                }));
 
 
 
 
 
-    }
+
 
     @Override
     public void addContentView(View view, ViewGroup.LayoutParams params) {
         super.addContentView(view, params);
     }
-    @Override
-    protected void onStop() {
-        super.onStop();
-        disposable.clear();
-    }
+
 }
 
 
